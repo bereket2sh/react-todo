95d60e1a1c0a52c7a773518aaecaa67540fb3d73
bereket2sh <bereketshimels77@gmail.com>
2025-04-26 11:38:58 -0500

    using props to pass data to the component

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,26 +1,33 @@
-function ListGroup() {
-  let items = [
-    "An item",
-    "A second item",
-    "A third item",
-    "A fourth item",
-    "And a fifth one",
-  ];
+import { useState } from "react";
 
-  if (items.length === 0) {
-    return (
-      <>
-        <h1> List </h1> <p> No items found</p>
-      </>
-    );
-  }
+//using props to pass data to the component
+interface Props {
+  items: string[];
+  heading: string;
+}
 
+function ListGroup({ items, heading }: Props) {
+  //intially no item is selected
+  let [selectedIndex, setSelectedIndex] = useState(-1);
   return (
     <>
-      <h1> List </h1>
+      <h1> {heading} </h1>
+      {items.length === 0 && <p> No items found</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item}>{item}</li>
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </>
